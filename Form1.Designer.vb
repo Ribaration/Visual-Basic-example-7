2bdf875e245cf6ee05f202fd88beab561f699990
Dell-User <Dell-User@DESKTOP-221INCO>
2024-01-17 09:59:33 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.ControlDark
+        Label1.BorderStyle = BorderStyle.Fixed3D
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(55, 62)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(141, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Length"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = SystemColors.ControlDark
+        Label2.BorderStyle = BorderStyle.FixedSingle
+        Label2.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(57, 112)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(139, 32)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter Height"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlDark
+        Button1.BackgroundImageLayout = ImageLayout.Center
+        Button1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(55, 173)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(139, 54)
+        Button1.TabIndex = 2
+        Button1.Text = "Area is"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ControlDark
+        Button2.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(55, 233)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(139, 56)
+        Button2.TabIndex = 3
+        Button2.Text = "Perimeter is"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(217, 62)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(217, 130)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(217, 191)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(217, 252)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaptionText
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        ForeColor = SystemColors.ActiveCaptionText
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+
+End Class
